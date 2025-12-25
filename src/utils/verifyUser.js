abf0e4c4abb8342c82c15259150fe3b721540120
Amir Sohel Sardar <sohelamirsohel786@gmail.com>
2025-12-25 23:20:15 +0530

    photo

--- src/utils/verifyUser.js
+++ src/utils/verifyUser.js
@@ -1,10 +1,16 @@
 import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
+
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-  if (!token) {
+  // ✅ Read token from Authorization header
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return next(errorHandler(401, 'Unauthorized'));
   }
+
+  const token = authHeader.split(' ')[1];
+
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return next(errorHandler(401, 'Unauthorized'));
