500d8ac4ca5e039d70084cc00b540d2b2fa4854f
Amir Sohel Sardar <sohelamirsohel786@gmail.com>
2026-03-11 02:33:52 +0530

    initial commitxc

--- src/routes/visitor.route.js
+++ src/routes/visitor.route.js
@@ -4,5 +4,6 @@ import { trackVisitor } from '../controllers/visitor.controller.js';
 const router = express.Router();
 
 router.get('/', trackVisitor);
+router.post('/', trackVisitor);
 
 export default router;
